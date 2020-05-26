1fdac8e070dd7572d6753beae636bf747cb4e1a8
Otto Seiskari <otto.seiskari@gmail.com>
2020-05-26 18:02:52 +0300

    Fix build with USE_SLAM=false and add 'namespace' prefix to internal CMake variables

--- scripts/mobile-cv-suite-config.cmake
+++ scripts/mobile-cv-suite-config.cmake
@@ -1,97 +1,113 @@
 add_library(mobile-cv-suite STATIC IMPORTED GLOBAL)
 
+# _MCS_ = Mobile CV Suite. Note that CMake variables are global by
+# default and will mess up other build scripts on conflicts
+
 if (ANDROID)
-  set(OPENCV_LIB_EXT "a")
-  set(PREBUILT_DIR "${CMAKE_CURRENT_LIST_DIR}/build/android")
-  set(LIB_DIR "${PREBUILT_DIR}/lib/${ANDROID_ABI}")
+  set(_MCS_OPENCV_LIB_EXT "a")
+  set(_MCS_PREBUILT_DIR "${CMAKE_CURRENT_LIST_DIR}/build/android")
+  set(_MCS_LIBS "${_MCS_PREBUILT_DIR}/lib/${ANDROID_ABI}")
 else()
-  set(OPENCV_LIB_EXT "so")
-  set(PREBUILT_DIR "${CMAKE_CURRENT_LIST_DIR}/build/host")
-  set(LIB_DIR "${PREBUILT_DIR}/lib")
+  set(_MCS_OPENCV_LIB_EXT "so")
+  set(_MCS_PREBUILT_DIR "${CMAKE_CURRENT_LIST_DIR}/build/host")
+  set(_MCS_LIBS "${_MCS_PREBUILT_DIR}/lib")
 endif()
 
-set(INC_DIR "${PREBUILT_DIR}/include")
-set(INTERFACE_INCLUDES
-  ${INC_DIR}
-  ${INC_DIR}/eigen3)
+set(_MCS_INC "${_MCS_PREBUILT_DIR}/include")
+set(_MCS_INTERFACE_INCLUDES
+  ${_MCS_INC}
+  ${_MCS_INC}/eigen3)
 
-set(INTERFACE_LIBS
+set(_MCS_INTERFACE_LIBS
   # --- static
-  ${LIB_DIR}/libamd.a
-  ${LIB_DIR}/libbtf.a
-  ${LIB_DIR}/libcamd.a
-  ${LIB_DIR}/libccolamd.a
-  ${LIB_DIR}/libcolamd.a
-  ${LIB_DIR}/libcxsparse.a
-  #${LIB_DIR}/libdbow2.a
-  ${LIB_DIR}/libg2o_core.a
-  ${LIB_DIR}/libg2o_solver_csparse.a
-  ${LIB_DIR}/libg2o_solver_dense.a
-  ${LIB_DIR}/libg2o_solver_eigen.a
-  ${LIB_DIR}/libg2o_solver_pcg.a
-  ${LIB_DIR}/libg2o_solver_slam2d_linear.a
-  ${LIB_DIR}/libg2o_solver_structure_only.a
-  ${LIB_DIR}/libg2o_stuff.a
-  ${LIB_DIR}/libg2o_types_data.a
-  ${LIB_DIR}/libg2o_types_icp.a
-  ${LIB_DIR}/libg2o_types_sba.a
-  ${LIB_DIR}/libg2o_types_sclam2d.a
-  ${LIB_DIR}/libg2o_types_sim3.a
-  ${LIB_DIR}/libg2o_types_slam2d.a
-  ${LIB_DIR}/libg2o_types_slam2d_addons.a
-  ${LIB_DIR}/libg2o_types_slam3d.a
-  ${LIB_DIR}/libg2o_types_slam3d_addons.a
-  ${LIB_DIR}/libopenblas.a
-  ${LIB_DIR}/libjsonl-recorder.a
-  ${LIB_DIR}/libsuitesparseconfig.a
-  ${LIB_DIR}/libtheia.a
-  ${LIB_DIR}/libyaml-cpp.a
-  # --- shared
-  ${LIB_DIR}/libmetis.so
-  ${LIB_DIR}/libg2o_csparse_extension.so
-  ${LIB_DIR}/libsuitesparseconfig.so
+  ${_MCS_LIBS}/libtheia.a
   # --- opencv
-  ${LIB_DIR}/libopencv_videoio.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_video.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_calib3d.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_features2d.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_imgproc.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_imgcodecs.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_flann.${OPENCV_LIB_EXT}
-  ${LIB_DIR}/libopencv_core.${OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_videoio.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_video.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_calib3d.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_features2d.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_imgproc.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_imgcodecs.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_flann.${_MCS_OPENCV_LIB_EXT}
+  ${_MCS_LIBS}/libopencv_core.${_MCS_OPENCV_LIB_EXT}
 )
 
+if (EXISTS ${_MCS_LIBS}/libamd.a)
+  set(_MCS_USE_SLAM TRUE)
+else()
+  set(_MCS_USE_SLAM FALSE)
+endif()
+
+if (_MCS_USE_SLAM)
+  list(APPEND _MCS_INTERFACE_LIBS
+    # --- static
+    ${_MCS_LIBS}/libamd.a
+    ${_MCS_LIBS}/libbtf.a
+    ${_MCS_LIBS}/libcamd.a
+    ${_MCS_LIBS}/libccolamd.a
+    ${_MCS_LIBS}/libcolamd.a
+    ${_MCS_LIBS}/libcxsparse.a
+    ${_MCS_LIBS}/libdbow2.a
+    ${_MCS_LIBS}/libg2o_core.a
+    ${_MCS_LIBS}/libg2o_solver_csparse.a
+    ${_MCS_LIBS}/libg2o_solver_dense.a
+    ${_MCS_LIBS}/libg2o_solver_eigen.a
+    ${_MCS_LIBS}/libg2o_solver_pcg.a
+    ${_MCS_LIBS}/libg2o_solver_slam2d_linear.a
+    ${_MCS_LIBS}/libg2o_solver_structure_only.a
+    ${_MCS_LIBS}/libg2o_stuff.a
+    ${_MCS_LIBS}/libg2o_types_data.a
+    ${_MCS_LIBS}/libg2o_types_icp.a
+    ${_MCS_LIBS}/libg2o_types_sba.a
+    ${_MCS_LIBS}/libg2o_types_sclam2d.a
+    ${_MCS_LIBS}/libg2o_types_sim3.a
+    ${_MCS_LIBS}/libg2o_types_slam2d.a
+    ${_MCS_LIBS}/libg2o_types_slam2d_addons.a
+    ${_MCS_LIBS}/libg2o_types_slam3d.a
+    ${_MCS_LIBS}/libg2o_types_slam3d_addons.a
+    ${_MCS_LIBS}/libopenblas.a
+    ${_MCS_LIBS}/libsuitesparseconfig.a
+    ${_MCS_LIBS}/libyaml-cpp.a
+    # --- shared
+    ${_MCS_LIBS}/libmetis.so
+    ${_MCS_LIBS}/libg2o_csparse_extension.so
+    ${_MCS_LIBS}/libsuitesparseconfig.so)
+endif()
+
 if (ANDROID)
-  list(APPEND INTERFACE_LIBS
-    ${LIB_DIR}/libtegra_hal.a
+  list(APPEND _MCS_INTERFACE_LIBS
+    ${_MCS_LIBS}/libtegra_hal.a
     z) # zlib, required by OpenCV stuff
 else()
+  if (_MCS_USE_SLAM)
+    list(APPEND _MCS_INTERFACE_LIBS ${_MCS_LIBS}/libpangolin.so)
+  endif()
+
   find_package(Threads REQUIRED)
-  list(APPEND INTERFACE_LIBS
-    ${LIB_DIR}/libopencv_highgui.so
-    ${LIB_DIR}/libpangolin.so
-    ${LIB_DIR}/libloguru.a
+  list(APPEND _MCS_INTERFACE_LIBS
+    ${_MCS_LIBS}/libopencv_highgui.so
+    ${_MCS_LIBS}/libloguru.a
     Threads::Threads)
 
   # Linux needs more libraries for logging
-  find_library(DL_LIBRARY NAMES dl)
-  if (EXISTS ${DL_LIBRARY})
-    message(STATUS "the dl (dynamic linking) library ${DL_LIBRARY} exists, adding it for loguru")
-    list(APPEND INTERFACE_LIBS ${DL_LIBRARY})
+  find_library(_MCS_DL_LIBRARY NAMES dl)
+  if (EXISTS ${_MCS_DL_LIBRARY})
+    message(STATUS "the dl (dynamic linking) library ${_MCS_DL_LIBRARY} exists, adding it for loguru")
+    list(APPEND _MCS_INTERFACE_LIBS ${_MCS_DL_LIBRARY})
   endif()
 
   # not sure why OpenCV likes the folder opencv4/opencv2
-  list(APPEND INTERFACE_INCLUDES ${INC_DIR}/opencv4)
+  list(APPEND _MCS_INTERFACE_INCLUDES ${_MCS_INC}/opencv4)
 endif()
 
 # Fortran may be needed on some platforms.
 find_library(GFORTRAN_LIBRARY NAMES gfortran)
 if (EXISTS ${GFORTRAN_LIBRARY})
   message(STATUS "gfortan found")
-  list(APPEND INTERFACE_LIBS ${GFORTRAN_LIBRARY})
+  list(APPEND _MCS_INTERFACE_LIBS ${GFORTRAN_LIBRARY})
 endif()
 
 set_target_properties(mobile-cv-suite PROPERTIES
-    IMPORTED_LOCATION "${LIB_DIR}/libdbow2.a" # any library will do
-    IMPORTED_LINK_INTERFACE_LIBRARIES "${INTERFACE_LIBS}"
-    INTERFACE_INCLUDE_DIRECTORIES "${INTERFACE_INCLUDES}")
+    IMPORTED_LOCATION "${_MCS_LIBS}/libjsonl-recorder.a" # any library will do
+    IMPORTED_LINK_INTERFACE_LIBRARIES "${_MCS_INTERFACE_LIBS}"
+    INTERFACE_INCLUDE_DIRECTORIES "${_MCS_INTERFACE_INCLUDES}")
