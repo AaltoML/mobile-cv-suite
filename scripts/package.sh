264f8b41c6a5f36ab9feb51f690720fa843192fd
Otto Seiskari <otto.seiskari@gmail.com>
2020-08-14 19:12:23 +0300

    Also package Android build artefacts

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,10 +2,11 @@
 # Package built artefacts as an archive file
 set -eux
 # TODO: OSX build
-DIR=build/host
-(find $DIR/include/ \
-  && find $DIR/lib/ -name "*.a*" -o -name "*.so*" \
-  && find $DIR/share/doc/ && find $DIR/share/licenses/ &&
-  echo scripts/mobile-cv-suite-config.cmake &&
+DIR=build
+(find $DIR/host/include/ \
+  && find $DIR/host/lib/ -name "*.a*" -o -name "*.so*" \
+  && find $DIR/host/share/doc/ -type f && find $DIR/host/share/licenses/ -type f \
+  && find $DIR/android/ -type f \
+  && echo scripts/mobile-cv-suite-config.cmake && \
   echo mobile-cv-suite-config.cmake) | \
-  tar -cf build/mobile-cv-suite.tar.gz -T -
+  tar -c -T - | gzip --best > build/mobile-cv-suite.tar.gz
